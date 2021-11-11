78c5aaf0f884d54a8261812023617f3615b9977a
unknown <nguyensongvo2652001@gmail.com>
2021-11-11 10:14:47 -0800

    Added routes for getting followers and followings of an user

--- controllers/followController.js
+++ controllers/followController.js
@@ -1,5 +1,6 @@
 const Follow = require('../models/followModel');
 const AppError = require('../utils/appError');
+const catchAsync = require('../utils/catchAsync');
 
 const handlerFactory = require('./handlerFactory');
 
@@ -23,6 +24,40 @@ const checkFollowUser = async (req, res, next) => {
   next();
 };
 
+const getFollowers = catchAsync(async (req, res, next) => {
+  const followers = await Follow.find({ following: req.params.id })
+    .select('id user')
+    .populate({
+      path: 'user',
+      select: 'username email'
+    });
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      length: followers.length,
+      followers
+    }
+  });
+});
+
+const getFollowings = catchAsync(async (req, res, next) => {
+  const followings = await Follow.find({ user: req.params.id })
+    .select('id following')
+    .populate({
+      path: 'following',
+      select: 'username email'
+    });
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      length: followings.length,
+      followings
+    }
+  });
+});
+
 const createFollow = handlerFactory.createOne(Follow, 'follow');
 
 const deleteFollow = handlerFactory.deleteOne(Follow, 'follow');
@@ -31,5 +66,7 @@ module.exports = {
   createFollow,
   setFollowRequestBody,
   deleteFollow,
-  checkFollowUser
+  checkFollowUser,
+  getFollowers,
+  getFollowings
 };
--- routers/userRouter.js
+++ routers/userRouter.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const authController = require('../controllers/authController');
+const followController = require('../controllers/followController');
 
 const router = express.Router();
 
 router.post('/signup', authController.signUp);
 router.post('/login', authController.login);
+
+router.use(authController.protect);
+router.get('/:id/followers', followController.getFollowers);
+router.get('/:id/followings', followController.getFollowings);
 
 module.exports = router;
